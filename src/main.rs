34cecbe0d207d4f33eaedb65c4453a0cd5bcb9a5
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2022-07-29 23:21:47 +0700

    Rewrite and update dependency to use clap builder API for design reason

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
-use clap::{Parser, Subcommand};
+use clap::{arg, Command};
 
-#[derive(Parser)]
-#[clap(version, about)]
-struct Cli {
-    #[clap(subcommand)]
-    command: Commands,
-}
+fn main() {
+    let app = Command::new("stalker")
+        .version("0.1.0")
+        .author("Peter <peterajaaa@gmail.com>")
+        .about("stalker is a fast and simple file watcher and executor with git-like syntax.")
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("init")
+            .about("Initialize a stalker instance")
+            )
+        .subcommand(
+            Command::new("add")
+            .about("Add path(s) to the stalk-list")
+            .arg_required_else_help(true)
+            .arg(
+                arg!([PATH])
+                .required(true)
+                .takes_value(true)
+                .multiple_values(true)
+                )
+            )
+        .get_matches();
 
-#[derive(Subcommand)]
-enum Commands {
-    Init {},
-    Add{
-        #[clap(value_parser)]
-        filepath: Option<String>,
-    },
-    Remove {
-        #[clap(value_parser)]
-        filepath: Option<String>,
-    },
-}
-
-fn main () {
-    let cli = Cli::parse();
-}
+    match app.subcommand() {
+        Some(("init", _init_subcommand)) => {
+            println!("stalker initialized...");
+        }
+        Some(("add", add_subcommand)) => {
+            let paths: Vec<_> = add_subcommand.get_many::<String>("PATH").unwrap().collect();
+            println!("{:?}", paths);
+        }
+        _ => () //Done because every subcommand should raise help on error.
+    }
+    }
