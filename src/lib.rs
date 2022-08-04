4f7c5dadd3295bc559320ca8013ffb44d6a50e11
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2022-08-04 17:53:13 +0700

    Add create_stalk_list() and update_stalk_list()

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use std::{fs, path::PathBuf};
+use std::io::{BufWriter, Write};
+use std::{
+    fs::{self, OpenOptions},
+    path::PathBuf,
+};
 use termion::{color, style};
 
 pub fn create_stalker_dir(path: &PathBuf) {
@@ -16,5 +20,60 @@ pub fn create_stalker_dir(path: &PathBuf) {
             color::Fg(color::Red),
             e
         ),
+    }
+}
+
+// TODO: Find a way to open file only once so I/O isn't going to be expensive at big input (e.g.
+// 100, 1000, 10000+ paths)
+pub fn create_stalk_list(stalker_instance: PathBuf, input_path: &String) {
+    match fs::write(stalker_instance.join("stalklist.txt"), input_path) {
+        Ok(_) => println!(
+            "{}{}Successfully added {} to stalklist.",
+            style::Bold,
+            color::Fg(color::Green),
+            input_path
+        ),
+        Err(e) => eprintln!(
+            "{}{}{}Error adding {} to stalklist: {}",
+            style::Bold,
+            style::Italic,
+            color::Fg(color::Red),
+            input_path,
+            e
+        ),
+    }
+}
+
+pub fn update_stalk_list(stalker_instance: PathBuf, input_path: &String) {
+    match OpenOptions::new()
+        .append(true)
+        .open(stalker_instance.join("stalklist.txt"))
+    {
+        Ok(mut file) => match write!(file, "{}", input_path) {
+            Ok(_) => println!(
+                "{}{}Successfully added {} to stalklist.",
+                style::Bold,
+                color::Fg(color::Green),
+                input_path
+            ),
+            Err(e) => eprintln!(
+                "{}{}{}Error adding {} to stalklist: {}",
+                style::Bold,
+                style::Italic,
+                color::Fg(color::Red),
+                input_path,
+                e
+            ),
+        },
+        Err(e) => {
+            eprintln!(
+                "{}{}{}Error opening stalklist at {}: {}",
+                style::Bold,
+                style::Italic,
+                color::Fg(color::Red),
+                input_path,
+                e
+            )
+        }
     }
 }
